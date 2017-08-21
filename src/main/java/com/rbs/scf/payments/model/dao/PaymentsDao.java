fe165439f084427de89a1f5026b8f6a7e9b73891
aakritiomar <30442269+aakritiomar@users.noreply.github.com>
2017-08-21 09:10:47 +0530

    Update PaymentsDao.java

--- src/main/java/com/rbs/scf/payments/model/dao/PaymentsDao.java
+++ src/main/java/com/rbs/scf/payments/model/dao/PaymentsDao.java
@@ -2,24 +2,29 @@ package com.rbs.scf.payments.model.dao;
 
 import com.rbs.scf.payments.model.beans.*;
 
+
 public interface PaymentsDao {
 //return all details
-	Bank[] getAllBankDetails();
-	Swift[] getAllSwiftDetails();
-	Customer_Transaction[] getAllCustomerTransactionDetails();
-	Bank_to_Customer[] getAllBankToCustomerDetails();
-	Customer_to_Bank[] getAllCustomerToBankDetails();
-	//return row corresponding to a key
-	Bank getBankDetails(String swift_id);
-	Swift getSwiftDetails(int transaction_id);
-	Customer_Transaction getCustomerTransactionDetails(int transaction_id);
-	Bank_to_Customer getBankToCustomerDetails(int transaction_id);
-	Customer_to_Bank getCustomerToBankDetails(int transaction_id);
-	//inserting a row to database
-	boolean addBank(Bank b);
-	boolean addSwift(Swift s);
-	boolean addCustomer_Transaction(Customer_Transaction ct);
-	boolean addBank_to_Customer(Bank_to_Customer btc);
-	boolean addCustomer_to_Bank(Customer_to_Bank ctb);
+Bank[] getAllBankDetails();
+Swift[] getAllSwiftDetails();
+Customer_Transaction[] getAllCustomerTransactionDetails();
+Bank_to_Customer[] getAllBankToCustomerDetails();
+Customer_to_Bank[] getAllCustomerToBankDetails();
+//return row corresponding to a key
+Bank getBankDetails(String swift_id);
+Swift getSwiftDetails(int transaction_id);
+Customer_Transaction getCustomerTransactionDetails(int transaction_id);
+Bank_to_Customer getBankToCustomerDetails(int transaction_id);
+Customer_to_Bank getCustomerToBankDetails(int transaction_id);
+//inserting a row to database
+boolean addBank(Bank b);
+boolean addSwift(Swift s);
+boolean addCustomer_Transaction(Customer_Transaction ct);
+boolean addBank_to_Customer(Bank_to_Customer btc);
+boolean addCustomer_to_Bank(Customer_to_Bank ctb);
 
+//to extract details of pending transactions
+Customer_Transaction[] getAllPendingCustomerTransactionDetails();
+Bank_to_Customer[] getAllPendingBankToCustomerDetails();
+Customer_to_Bank[] getAllPendingCustomerToBankDetails();
 }
